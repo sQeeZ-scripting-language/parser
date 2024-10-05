83b15a5eb8bf462e1d0a0f8acf971a577937ef35
marcelnoehre <marcelnoehre@web.de>
2024-10-05 23:26:20 +0200

    parser class

---
+++ include/parser/parser.hpp
@@ -0,0 +1,16 @@
+#ifndef PARSER_HPP
+#define PARSER_HPP
+
+#include <iostream>
+
+#include <lexer.hpp>
+
+class Parser {
+public:
+    Parser(const std::string& code);
+    void parse(bool devMode);
+
+    const std::string code;
+};
+
+#endif
---
+++ src/parser/parser.cpp
@@ -0,0 +1,7 @@
+#include "parser.hpp"
+
+Parser::Parser(const std::string& code) : code(code) { }
+
+void Parser::parse(bool devMode) {
+    
+}
